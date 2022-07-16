6e52c59274d11dbbb712e3f74c072be0824467d4
kesaviwebsolutions <89696473+mynkbhati22@users.noreply.github.com>
2022-07-16 17:04:11 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,56 +1,119 @@
-import React from 'react'
-import img from "./images/favicon.ico"
+import React from "react";
+import { useState } from "react";
 
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 
+import Brightness2Icon from "@mui/icons-material/Brightness2";
+import BrightnessLowIcon from "@mui/icons-material/BrightnessLow";
 
-// const = [] //array
-// const = [
-//     {
-//         // objects
-//     }
-// ]
+import Slider from "./components/SliderSmallThumb";
+
+function valuetext(value) {
+  return `${value}°C`;
+}
 
 export default function App() {
-  const firstArray = [
-    {
-      img: [img],
-      title: "Card title",
-      desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      subdesc: "Go somewhere",
-    },
-    {
-      img: [img],
-      title: "Card title",
-      desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      subdesc: "Go somewhere",
-    },
-    {
-      img: [img],
-      title: "Card tite",
-      desc: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-      subdesc: "Go somewhere",
-    },
-    
-  ]
-  const renderArray = (firstArray, index) => {
-    return (
-      <div className='col-lg-4 col-md-6' key={index}>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={firstArray.img} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{firstArray.title}</h5>
-            <p className="card-text">{firstArray.desc}</p>
-            <a href="/" className="btn btn-primary">{firstArray.subdesc}</a>
-          </div>
-        </div>
-      </div>
-    );
+  const [value, setValue] = useState(25);
 
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
   };
+
+  const [value2, setValue2] = useState(50);
+
+  const handleSliderChange2 = (event, newValue) => {
+    setValue2(newValue);
+  };
+
+  const [value3, setValue3] = React.useState([20, 37]);
+
+  const handleSliderChange3 = (event, newValue) => {
+    setValue3(newValue);
+  };
+
   return (
-    <div>
-      <div className='row'>{console.log(firstArray)}
-        {firstArray.map(renderArray)}</div>
-    </div>
-  )
+    <Box p={"20px"} sx={{ border: "1px solid gray" }}>
+      <Stack
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Brightness2Icon sx={{ color: value >= 75 ? "#ffc107" : "#B8DDFB" }} />
+        <Slider
+          value={typeof value === "number" ? value : 0}
+          onChange={handleSliderChange}
+          sx={{
+            width: 220,
+            "& .MuiSlider-rail": {
+              backgroundColor: value >= 75 ? "#ffe082" : "#B8DDFB",
+            },
+            "& .MuiSlider-track:before": {
+              backgroundColor: value >= 75 ? "#ffc107" : "primary.main",
+            },
+          }}
+        />
+
+        <BrightnessLowIcon
+          sx={{ color: value >= 75 ? "#ffc107" : "#B8DDFB" }}
+        />
+      </Stack>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        Disabled
+        <Slider
+          disabled
+          value={typeof value2 === "number" ? value2 : 0}
+          onChange={handleSliderChange2}
+          sx={{
+            width: 220,
+            "& .MuiSlider-rail": {
+              backgroundColor: value2 >= 75 ? "#ffe082" : "#B8DDFB",
+            },
+            "& .MuiSlider-track:before": {
+              backgroundColor: value2 >= 75 ? "#ffc107" : "primary.main",
+            },
+          }}
+        />
+      </Stack>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        marks
+        <Slider
+          aria-label="Small steps"
+          defaultValue={0.00000005}
+          getAriaValueText={valuetext}
+          step={0.00000001}
+          marks
+          min={-0.00000005}
+          max={0.0000001}
+          valueLabelDisplay="auto"
+        />
+      </Stack>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        range slider
+        <Slider
+          getAriaLabel={() => "Temperature range"}
+          value={value3}
+          onChange={handleSliderChange3}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+        />
+      </Stack>
+    </Box>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals'; 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { StrictMode } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
+import SideNavbar from "./components/SideNavbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <SideNavbar />
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
